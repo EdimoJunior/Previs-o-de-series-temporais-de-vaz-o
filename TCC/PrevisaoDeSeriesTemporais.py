e8a096912bc3378b0052a7d6626070220ba71dee
Edimo Junior <32398526+EdimoJunior@users.noreply.github.com>
2019-10-02 01:14:49 -0300

    Organizadoa dados, entrada e saida
    colocado a entrada no padrão de matriz e a saida em um vetor.

--- TCC/PrevisaoDeSeriesTemporais.py
+++ TCC/PrevisaoDeSeriesTemporais.py
@@ -1,38 +1,101 @@
 # Import `pyplot` from `matplotlib`
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import torch
 
 #importando a tabela com o pandas
-tabela = pd.read_csv('vazoes_C_60855000.csv', delimiter=',', names = ['Data', 'Maxima', 'Minima', 'Media'])
+df = pd.read_csv('vazoes_C_60855000_2.csv', delimiter=',', names = ['Data', 'Maxima', 'Minima', 'Media'])
+#print(df)
 
-#vetor p/ armazenar datas
-vetDatas = ["" for x in range(13)]
-#print(vetDatas)
+#Separando as colunas que serão utilizadas pela rede neural...
+colunaMedia = df[['Media']]
+colunaData = df[['Data']]
+#print(colunaMedia)
 
-#vetor p/ armazenar médias
-vetMedias = [0] * 13
-#print(vetMedias)
+#Separando os dados para treinamento e dados de testes...
+training = colunaMedia.iloc[822:947]
+test = colunaMedia.iloc[935:947] #não utilizado
+#print(training)
+#print(test)
 
-aux = 0
+#Separando as entradas e saidas do treinamento da rede...
+inicio = 0
+fim = 12
+training_input1 = np.empty([np.size(training) - 0, 12])
+training_output1 = []
 
-# preenchendo o vetor de datas
-for i, j in tabela.iterrows():
-    if i >= 52 and i <=63:
-        aux = aux + 1
-        #print("Linha = ",i, "\n",j['Data'])
-        vetDatas[aux] = j['Data']
-        # print("Linha = ",i, "\n",j['Media'])
-        vetMedias[aux] = j['Media']
-        #print()
+for i in range(5): #quantos meses vae ser pegos
 
-# Initialize the plot
-fig = plt.figure(figsize=(20,10))
-ax1 = fig.add_subplot(111)
+    training_input = np.append(training.iloc[inicio:fim], [])
+    print("\n", training_input) #print to check
+    for j in range(12): #tamanho da janela
+        training_input1[i,j]=training_input[j]
 
-# or replace the three lines of code above by the following line:
+    training_output = np.append(training.iloc[fim],[])
+    print("\n", training_output) #print to check
+    training_output1.append(training.iloc[fim])
 
-# Plot the data
-ax1.bar(vetDatas,vetMedias)
+    inicio = inicio+1
+    fim = fim+1
 
-# Show the plot
-plt.show()
+print("")
+for i in range(5): #quantos meses vae ser pegos
+    for j in range(12): #tamanho da janela
+        print(round(training_input1[i,j], 2)," ", end = '')
+    print("")
+
+print("\n", training_output1)
+
+print("\nEntrada --> ", training_input1[0]) #Imprimindo toda a linha da matriz
+print("Saida --> ", training_output1[0].Media) #imprimindo o primeiro valor do resultado da primeira entrada.
+
+'''class Net(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(Net, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+    def forward(self, x):
+        hidden = self.fc1
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        output = self.sigmoid(output)
+        return output
+
+#convert to tensors
+training_input1 = torch.FloatTensor(training_input1)
+training_output1 = torch.FloatTensor(training_output1)
+
+input_size = training_input1.__sizeof__()# number of features selected
+hidden_size = 30 # number of nodes/neurons in the hidden layer
+model = Net(input_size, hidden_size) # create the model
+#criterion = tc.nn.BCELoss() # works for binary classification
+criterion = torch.nn.ReLU
+# without momentum parameter
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.9)
+#with momentum parameter
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.9, momentum=0.2)
+
+model.eval()
+y_pred = model(training_input1)
+before_train = criterion(y_pred.squeeze(), training_output1)
+print('Test loss before training' , before_train.item())
+
+model.train()
+epochs = 5000
+errors = []
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    # Forward pass
+    y_pred = model(training_input1)
+    # Compute Loss
+    loss = criterion(y_pred.squeeze(), training_output1)
+    errors.append(loss.item())
+    print('Epoch {}: train loss: {}'.format(epoch, loss.item()))
+    # Backward pass
+    loss.backward()
+    optimizer.step()'''
